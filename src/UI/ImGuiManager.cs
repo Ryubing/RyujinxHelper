8e2c5de776490b810d10286a854a9f26a1ecaa22
Evan Husted <gr33m11@gmail.com>
2024-07-12 10:20:29 -0500

    ImGui has some more code made for future use, such as ImGuiLayerState Updated ico Background color picker test

--- src/UI/ImGuiManager.cs
+++ src/UI/ImGuiManager.cs
@@ -1,22 +1,24 @@
-﻿using Silk.NET.Input;
+﻿using System.Numerics;
+using Silk.NET.Input;
 using Silk.NET.OpenGL;
 using Silk.NET.OpenGL.Extensions.ImGui;
 using Silk.NET.Windowing;
+using Color = System.Drawing.Color;
 
 namespace Volte.UI;
 
 // adapted from https://github.com/dotnet/Silk.NET/blob/main/examples/CSharp/OpenGL%20Demos/ImGui/Program.cs
-public class ImGuiManager : IDisposable
+public class ImGuiManager<TState> : IDisposable where TState : ImGuiLayerState
 {
     private readonly IWindow window;
 
     private ImGuiController controller;
     private GL gl;
     private IInputContext inputContext;
-    
-    public ImGuiLayer Layer { get; }
 
-    public ImGuiManager(ImGuiLayer igLayer, Gommon.Optional<WindowOptions> windowOptions = default)
+    public ImGuiLayer<TState> Layer { get; }
+
+    public ImGuiManager(ImGuiLayer<TState> igLayer, Gommon.Optional<WindowOptions> windowOptions = default)
     {
         Layer = igLayer;
         window = Window.Create(windowOptions.OrElse(WindowOptions.Default));
@@ -28,8 +30,9 @@ public class ImGuiManager : IDisposable
                 window,
                 inputContext = window.CreateInput()
             );
+            Info(LogSource.UI, $"Window 0x{window.Handle:X} loaded");
         };
-        
+
         window.FramebufferResize += sz => gl.Viewport(sz);
 
         window.Closing += () =>
@@ -43,26 +46,13 @@ public class ImGuiManager : IDisposable
         {
             controller.Update((float)delta);
 
-            gl.ClearColor(System.Drawing.Color.FromArgb(255, (int)(.45f * 255), (int)(.55f * 255), (int)(.60f * 255)));
+            gl.ClearColor(ImGuiLayerState.Vector3ToColor(Layer?.State?.Background ?? ImGuiLayerState.DefaultBackground));
             gl.Clear((uint)ClearBufferMask.ColorBufferBit);
 
             Layer?.Render(delta);
-            
+
             controller.Render();
         };
-    }
-
-    public static Thread CreateUiThread(ImGuiLayer layer)
-    {
-        var thread = new Thread(() =>
-        {
-            using var manager = new ImGuiManager(layer);
-            manager.Run();
-        })
-        {
-            Name = "Volte UI Thread"
-        };
-        return thread;
     }
 
     public void Run() => window.Run();
@@ -70,7 +60,38 @@ public class ImGuiManager : IDisposable
     void IDisposable.Dispose() => window?.Dispose();
 }
 
-public abstract class ImGuiLayer
+public static class ImGuiManager
 {
+    public static Thread CreateUiThread<TState>(ImGuiLayer<TState> layer) where TState : ImGuiLayerState
+    {
+        var thread = new Thread(() =>
+        {
+            using var manager = new ImGuiManager<TState>(layer);
+            manager.Run();
+        })
+        {
+            Name = "Volte UI Thread"
+        };
+        return thread;
+    }
+}
+
+public abstract class ImGuiLayer<TState> where TState : ImGuiLayerState
+{
+    public TState State { get; protected set; }
+
     public abstract void Render(double delta);
+}
+
+public abstract class ImGuiLayerState
+{
+    public static Vector3 DefaultBackground => new(.45f, .55f, .60f);
+
+    public Vector3 Background = DefaultBackground;
+
+    public static Color Vector3ToColor(Vector3 vec3)
+        => Color.FromArgb(255,
+            (int)(Math.Min(vec3.X, 1) * 255),
+            (int)(Math.Min(vec3.Y, 1) * 255),
+            (int)(Math.Min(vec3.Z, 1) * 255));
 }
--- src/UI/VolteImGuiLayer.cs
+++ src/UI/VolteImGuiLayer.cs
@@ -2,10 +2,24 @@
 
 namespace Volte.UI;
 
-public class VolteImGuiLayer : ImGuiLayer
+public class VolteImGuiLayer : ImGuiLayer<VolteImGuiLayer.VolteImGuiState>
 {
+    public VolteImGuiLayer()
+    {
+        State = new VolteImGuiState();
+    }
+
+    public sealed class VolteImGuiState : ImGuiLayerState;
+    
     public override void Render(double delta)
     {
-        ImGui.Text("Hello");
+        {
+            ImGui.Begin("UI Settings");
+            ImGui.ColorPicker3("Background", ref State.Background);
+            if (ImGui.Button("Reset Background"))
+                State.Background = ImGuiLayerState.DefaultBackground;
+            
+            ImGui.End();
+        }
     }
 }

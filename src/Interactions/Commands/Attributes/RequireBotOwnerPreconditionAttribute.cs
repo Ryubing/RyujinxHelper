f9417c3b36b66ba2fb3ceb0c0e17465391909a3d
Evan Husted <gr33m11@gmail.com>
2024-07-20 23:28:07 -0500

    Bot Owner, Admin & Mod interaction preconditions

---
+++ src/Interactions/Commands/Attributes/RequireBotOwnerPreconditionAttribute.cs
@@ -0,0 +1,18 @@
+﻿using Discord.Interactions;
+
+namespace Volte.Interactions.Commands;
+
+public class RequireBotOwnerPreconditionAttribute : PreconditionAttribute
+{
+    public override Task<PreconditionResult> CheckRequirementsAsync(
+        IInteractionContext context,
+        ICommandInfo commandInfo,
+        IServiceProvider services
+    )
+    {
+        return Task.FromResult(context.User.Id == Config.Owner
+            ? PreconditionResult.FromSuccess()
+            : PreconditionResult.FromError("Insufficient permission.")
+        );
+    }
+}
---
+++ src/Interactions/Commands/Attributes/RequireGuildModeratorPreconditionAttribute.cs
@@ -0,0 +1,25 @@
+﻿using Discord.Interactions;
+
+namespace Volte.Interactions.Commands;
+
+public class RequireGuildModeratorPreconditionAttribute : PreconditionAttribute
+{
+    public override Task<PreconditionResult> CheckRequirementsAsync(
+        IInteractionContext context,
+        ICommandInfo commandInfo,
+        IServiceProvider services
+    )
+    {
+        if (context.Guild is null)
+            return Task.FromResult(PreconditionResult.FromError("This command can only be executed in a guild."));
+
+        var db = services.Get<DatabaseService>();
+        var data = db.GetData(context.Guild.Id);
+
+        return Task.FromResult(
+            data.Configuration.Moderation.ModRole == context.User.Id
+                ? PreconditionResult.FromSuccess()
+                : PreconditionResult.FromError("This command requires you to be a moderator.")
+        );
+    }
+}

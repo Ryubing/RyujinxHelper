50cf4375859c17ac2f121442479ede04cb1204ea
Evan Husted <greem@greemdev.net>
2025-01-10 00:35:51 -0600

    columnindices ref-based csv reading

--- src/Bot/Core/Entities/CompatibilityCsv.cs
+++ src/Bot/Core/Entities/CompatibilityCsv.cs
@@ -1,31 +1,36 @@
-﻿using System.Globalization;
-using System.IO;
+﻿using System.IO;
 using nietras.SeparatedValues;
 
 namespace RyuBot.Entities;
 
-public class CompatibilityCsv
+public struct ColumnIndices(Func<ReadOnlySpan<char>, int> getIndex)
 {
     public const string TitleIdCol = "\"title_id\"";
     public const string GameNameCol = "\"game_name\"";
     public const string LabelsCol = "\"labels\"";
     public const string StatusCol = "\"status\"";
     public const string LastUpdatedCol = "\"last_updated\"";
-    
+
+    public readonly int TitleId = getIndex(TitleIdCol);
+    public readonly int GameName = getIndex(GameNameCol);
+    public readonly int Labels = getIndex(LabelsCol);
+    public readonly int Status = getIndex(StatusCol);
+    public readonly int LastUpdated = getIndex(LastUpdatedCol);
+}
+
+public class CompatibilityCsv
+{
     private readonly SepSpec _spec;
-    
+
     public CompatibilityCsv(SepReader reader)
     {
         _spec = reader.Spec;
-        var entries = new List<CompatibilityEntry>();
+        ColumnIndices columnIndices = new(reader.Header.IndexOf);
 
-        foreach (var row in reader)
-        {
-            entries.Add(new CompatibilityEntry(reader.Header, row));
-        }
-
-        Entries = entries.Where(x => !x.Status.IsNullOrEmpty())
-            .OrderBy(it => it.GameName).ToArray();
+        Entries = reader
+            .Enumerate(row => new CompatibilityEntry(ref columnIndices, row))
+            .OrderBy(it => it.GameName)
+            .ToArray();
     }
 
     public CompatibilityEntry[] Entries { get; }
@@ -38,52 +43,49 @@ public class CompatibilityCsv
         foreach (var compatEntry in Entries)
         {
             using var row = sepWriter.NewRow();
-            row[TitleIdCol].Set(compatEntry.TitleId.OrElse(string.Empty));
-            row[GameNameCol].Set($"\"{compatEntry.GameName}\"");
-            row[LabelsCol].Set(compatEntry.IssueLabels.JoinToString(';'));
-            row[StatusCol].Set(compatEntry.Status.ToLower());
+            row[ColumnIndices.TitleIdCol].Set(compatEntry.TitleId.OrElse(string.Empty));
+            row[ColumnIndices.GameNameCol].Set($"\"{compatEntry.GameName}\"");
+            row[ColumnIndices.LabelsCol].Set(compatEntry.Labels.JoinToString(';'));
+            row[ColumnIndices.StatusCol].Set(compatEntry.Status.ToLower());
             var le = compatEntry.LastEvent;
-            row[LastUpdatedCol].Set(
+            row[ColumnIndices.LastUpdatedCol].Set(
                 $"{le.Year}-{le.Month:00}-{le.Day:00} " +
                 $"{le.Hour:00}:{le.Minute:00}:{le.Second:00}.000"
-                );
+            );
         }
 
         if (newPath.TryGetParent(out var parent) && !parent.ExistsAsDirectory)
             Directory.CreateDirectory(parent.Path);
-        
+
         newPath.WriteAllText(sepWriter.ToString());
     }
 }
 
 public class CompatibilityEntry
 {
-    public CompatibilityEntry(SepReaderHeader header, SepReader.Row row)
+    public CompatibilityEntry(ref ColumnIndices indices, SepReader.Row row)
     {
-        if (row.ColCount != header.ColNames.Count)
-            throw new InvalidDataException($"CSV row {row.RowIndex} ({row.ToString()}) has mismatched column count");
-            
-        var titleIdRow = colStr(row[header.IndexOf(CompatibilityCsv.TitleIdCol)]);
-        TitleId = !string.IsNullOrEmpty(titleIdRow) 
-            ? titleIdRow 
+        var titleIdRow = colStr(row[indices.TitleId]);
+        TitleId = !string.IsNullOrEmpty(titleIdRow)
+            ? titleIdRow
             : default(Gommon.Optional<string>);
 
-        GameName = colStr(row[header.IndexOf(CompatibilityCsv.GameNameCol)]).Trim().Trim('"');
+        GameName = colStr(row[indices.GameName]).Trim().Trim('"');
 
-        IssueLabels = colStr(row[header.IndexOf(CompatibilityCsv.LabelsCol)]).Split(';');
-        Status = colStr(row[header.IndexOf(CompatibilityCsv.StatusCol)]).Capitalize();
+        Labels = colStr(row[indices.Labels]).Split(';');
+        Status = colStr(row[indices.Status]).Capitalize();
 
-        if (DateTime.TryParse(colStr(row[header.IndexOf(CompatibilityCsv.LastUpdatedCol)]), out var dt))
+        if (DateTime.TryParse(colStr(row[indices.LastUpdated]), out var dt))
             LastEvent = dt;
 
         return;
-            
+
         string colStr(SepReader.Col col) => col.ToString().Trim('"');
     }
-    
+
     public string GameName { get; }
     public Gommon.Optional<string> TitleId { get; }
-    public string[] IssueLabels { get; }
+    public string[] Labels { get; }
     public string Status { get; }
     public DateTime LastEvent { get; }
 
@@ -91,17 +93,22 @@ public class CompatibilityEntry
 
     public string FormattedIssueLabels => FormatIssueLabels(false);
 
-    public string FormatIssueLabels(bool markdown = true) => IssueLabels
+    public string FormatIssueLabels(bool markdown = true) => Labels
         .Where(it => !it.StartsWithIgnoreCase("status"))
-        .Select(it => GitHubHelper.FormatLabelName(it, markdown))
-        .JoinToString(", ");
+        .FormatCollection(
+            it => GitHubHelper.FormatLabelName(it, markdown),
+            separator: ", "
+        );
 
     public override string ToString()
     {
         var sb = new StringBuilder($"{nameof(CompatibilityEntry)}: {{");
+        if (TitleId.HasValue)
+            sb.Append($"{nameof(TitleId)}={TitleId}, ");
         sb.Append($"{nameof(GameName)}=\"{GameName}\", ");
-        sb.Append($"{nameof(TitleId)}={TitleId}, ");
-        sb.Append($"{nameof(IssueLabels)}=\"{IssueLabels}\", ");
+        sb.Append($"{nameof(Labels)}={
+            Labels.FormatCollection(x => $"\"{x}\"", separator: ", ", prefix: "[", suffix: "]")
+        }, ");
         sb.Append($"{nameof(Status)}=\"{Status}\", ");
         sb.Append($"{nameof(LastEvent)}=\"{LastEvent}\"");
         sb.Append('}');

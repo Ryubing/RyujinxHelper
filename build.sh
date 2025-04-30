a01a5e6a166c6b662bf6e8439280d3d768079692
GreemDev <greemdev@ryujinx.app>
2025-04-30 00:04:17 -0500

    ryubot is now hosted on x86_64

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 rm -rf build
-cd src/Bot && dotnet publish -o ../../build/bot -c Release -r linux-arm64 --self-contained true -p:PublishSingleFile=true
+cd src/Bot && dotnet publish -o ../../build/bot -c Release -r linux-x64 --self-contained true -p:PublishSingleFile=true
 cd ../../
-cd src/UI && dotnet publish -o ../../build/ui -c Release -r linux-arm64 --self-contained true -p:PublishSingleFile=true 
+cd src/UI && dotnet publish -o ../../build/ui -c Release -r linux-x64 --self-contained true -p:PublishSingleFile=true 

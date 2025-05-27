e39934fac19a172570709cd79fb1ed6c176b8ba8
GreemDev <greemdev@ryujinx.app>
2025-05-27 03:19:44 -0500

    [ci skip] fix compile warnings

--- src/Bot/Commands/Interactions/Attributes/RequireRyubingGuildPreconditionAttribute.cs
+++ src/Bot/Commands/Interactions/Attributes/RequireRyubingGuildPreconditionAttribute.cs
@@ -4,16 +4,19 @@ namespace RyuBot.Commands.Interactions;
 
 public class RequireRyubingGuildPreconditionAttribute : PreconditionAttribute
 {
-    public override async Task<PreconditionResult> CheckRequirementsAsync(
-        IInteractionContext context, 
-        ICommandInfo commandInfo, 
+    public override Task<PreconditionResult> CheckRequirementsAsync(
+        IInteractionContext context,
+        ICommandInfo commandInfo,
         IServiceProvider services)
     {
         if (context.Guild is null)
-            return PreconditionResult.FromError("This command must be run in a guild.");
-        
-        return context.Guild.Id is not 1294443224030511104
-            ? PreconditionResult.FromError($"This command can only be run in {Format.Url("Ryubing", "https://discord.gg/ryujinx")}.") 
-            : PreconditionResult.FromSuccess();
+            return Task.FromResult(PreconditionResult.FromError("This command must be run in a guild."));
+
+        return Task.FromResult(
+            context.Guild.Id is not 1294443224030511104
+                ? PreconditionResult.FromError(
+                    $"This command can only be run in {Format.Url("Ryubing", "https://discord.gg/ryujinx")}.")
+                : PreconditionResult.FromSuccess()
+        );
     }
 }
